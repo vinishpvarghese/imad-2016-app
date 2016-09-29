2f9939d4804b3d8d689de5e0733c8dc5dd0c8861
Vinish <vinishpvarghese@gmail.com>
2016-09-29 18:59:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', function (req, res) {
-  res.sendFile("Hi This is from Article 1");
+  res.sendFile('Hi This is from Article one');
 });
 
 app.get('/ui/style.css', function (req, res) {
